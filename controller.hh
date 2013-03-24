4a2dff482fceb7e971353eda7df800b1d4d3d54b
Ubuntu <ubuntu@ip-10-202-66-89.ec2.internal>
2013-03-24 01:44:16 +0000

    decent parameters, but bad with huge trace

--- controller.hh
+++ controller.hh
@@ -11,7 +11,7 @@ private:
   bool debug_; /* Enables debugging output */
 
   /* Add member variables here */
-  uint64_t the_estimated_rtt = 60;
+  uint64_t the_estimated_rtt = 72;
   float the_throughput = 0.0;
   uint64_t update_time = 0; //when to update
   uint64_t update_interval = 25; //how often to update throughput
